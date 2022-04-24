6b2d444a6c4da6eecd48bfc961be2d454c13e2d9
kimn <a9523023650@gmail.com>
2022-04-24 14:01:04 +0800

    [新增] 第一周作業

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -22,7 +22,7 @@ db.rooms.deleteMany({ name: "標準單人防昇級版" });
 db.rooms.findOne({ price: 1500 });
 // 尋找多筆
 db.rooms.find({ price: 1500 });
-db.posts.find({ type: group });
+db.posts.find({ type: "group" });
 
 // 資料篩選 - 1500 以下
 db.rooms.find({ price: { $lte: 1500 } });
@@ -33,10 +33,12 @@ db.rooms.find({ price: { $lte: 1500 }, rating: { $gte: 4.7 } });
 db.rooms.find({ name: /豪華/ });
 
 // 尋找資料 - find 保護欄位
-db.rooms.find({ name: /豪華/ }, { _id: 0, rating: 0 });
+db.rooms.find({ name: /豪華/ }, { _id: 0 });
 // 尋找資料 - 尋找陣列裡的值
 db.rooms.find({ payment: { $in: ["信用卡"] } });
 
+// 1. 搜尋 name 欄位為 “Ray Xu” 的 document 列表
+db.posts.find({ name: "Ray Xu" });
 // 2. 新增一筆 document，請全部欄位皆填寫
 db.posts.insertOne({
     name: "貼文姓名",
@@ -48,7 +50,50 @@ db.posts.insertOne({
     type: ["粉絲", "社團"],
     tags: "貼文標籤",
 });
+// 3. 新增多筆 document，請全部欄位皆填寫
+db.posts.insertMany([
+    {
+        name: "貼文姓名",
+        image: "貼文圖片",
+        likes: "按讚數",
+        content: "貼文內容",
+        comments: "留言數",
+        createdAt: "發文時間",
+        type: ["粉絲", "社團"],
+        tags: "貼文標籤",
+    },
+    {
+        name: "貼文姓名",
+        image: "貼文圖片",
+        likes: "按讚數",
+        content: "貼文內容",
+        comments: "留言數",
+        createdAt: "發文時間",
+        type: ["粉絲", "社團"],
+        tags: "貼文標籤",
+    },
+]);
+// 4. 修改一筆 document，filter 條件請用 _id 指定其中一筆資料，content 欄位調整為測試資料
+db.posts.updateOne({ _id: ObjectId("625ad1647650705ec8fe6faf") }, { $set: { content: "測試資料" } });
 // 5. 修改多筆 name 欄位為 "Ray Xu" 的 document 列表，content 欄位都調整為哈哈你看看你
 db.posts.updateMany({ name: " Ray Xu" }, { $set: { content: "哈哈你看看你" } });
+// 6. 刪除一筆 document，filter 條件請用 `_id` 任意指定其中一筆資料
+db.posts.deleteOne({ _id: ObjectId("625ad1647650705ec8fe6faf") });
+// 7. 刪除多筆 document，filter 條件請用 type 為 group 的值，刪除所有社團貼文
+db.posts.deleteMany({ type: "group" });
 // 8. 刪除多筆 document，filter 條件為以下條件
 db.posts.deleteMany({ name: "Ray Xu", likes: { $lte: 500 } });
+// 9. 查詢全部 posts 的 document 列表
+db.posts.find();
+// 10. 關鍵字搜尋 name 裡面含有 o 的 document 列表
+db.posts.find({ name: /o/ });
+// 11. 查詢 name 欄位為 "Ray Xu" ，filter 篩選出介於 500~1000(含) 個讚
+db.posts.find({ name: "Ray Xu", likes: { $gte: 500, $lte: 1000 } });
+// 12. 查詢 comments 有超過 500(含)以上的 document 列表
+db.posts.find({ tags: { $gte: 500 } });
+// 13. 查詢 tags 欄位，有 謎因 或(or) 幹話 的 document 列表
+db.posts.find({ $or: [{ tags: "謎因" }, { tags: "幹話" }] });
+// 14. 查詢 tags 欄位，有 幹話 的 document 列表，需隱藏 _id 欄位
+db.posts.find({ tags: "幹話" }, { _id: 0 });
+// 15. 請嘗試用 Mongo Shell 指令刪除全部 Documents
+db.posts.deleteMany({});
