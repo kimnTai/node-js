4e091b85262f490a232eddcfde70a31d994565bd
kimn <a9523023650@gmail.com>
2022-05-15 20:47:04 +0800

    [新增] 完成 week1 主線

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -97,3 +97,37 @@ db.posts.find({ $or: [{ tags: "謎因" }, { tags: "幹話" }] });
 db.posts.find({ tags: "幹話" }, { _id: 0 });
 // 15. 請嘗試用 Mongo Shell 指令刪除全部 Documents
 db.posts.deleteMany({});
+
+// 1. posts 所有 document 數量為？(回傳數字)
+db.posts.find().count();
+// 2. 請查詢 `name` 為 `Ray Xu` 的 document 列表，排序為由新到舊
+db.posts.find({ name: "Ray Xu" }).sort({ createdAt: -1 });
+// 3. 請查詢 name 為 Ray Xu 的 document 列表，顯示前 30 筆資料
+db.posts.find({ name: "Ray Xu" }).limit(30);
+// 4. 請查詢 name 為 Ray Xu ，顯示100(含) 個讚以上的前 30 筆 document 列表，時間排序由新到舊
+db.posts
+    .find({ name: "Ray Xu", likes: { $gte: 100 } })
+    .limit(30)
+    .sort({ createdAt: -1 });
+// 5. 請查詢 comments 超過 100 的 document 列表，跳過前 30 筆資料，再顯示 30 筆資料
+db.posts
+    .find({ comments: { $gt: 100 } })
+    .skip(30)
+    .limit(30);
+
+// 6. 尋找超夯熱門貼文，請查詢 likes 且(and) comments 都超過 1,500(含) 的 document 列表
+db.posts.find({
+    $and: [{ likes: { $gte: 1500 } }, { comments: { $gte: 1500 } }],
+});
+
+// 7. 尋找普通熱門貼文，請查詢 likes 或(or) comments 超過 1,000(含) 的 document 列表
+db.posts.find({
+    $or: [{ likes: { $gte: 1000 } }, { comments: { $gte: 1000 } }],
+});
+
+// 8. 查詢 `image` 欄位為 `null` 的 document 列表
+db.posts.find({ image: { $eq: null } });
+// 9. 隨意找一筆 document 資料，將 `tags` 欄位裡的陣列，新增一個新 tags 為 遊記
+db.posts.updateOne({ _id: ObjectId("6252ea069c8da3f354e88f34") }, { $push: { tags: { $each: ["遊記"] } } });
+// 10. 將所有 `tags` 陣列裡的 `感情` 都移除
+db.posts.updateMany({}, { $pullAll: { tags: ["感情"] } });
